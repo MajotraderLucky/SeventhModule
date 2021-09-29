fa3505adbaa680198ee0aea164ea82c11011c51c
Sergey_Ryazanov <majo78@mail.ru>
2021-09-29 21:40:48 +0300

    Change file CuckooFor.cpp

--- CuckooFor.cpp
+++ CuckooFor.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "What time is it now?\n";
+       int hour;
+    std::cin >> hour;
+     if ((hour > 0) && (hour <= 24)) {
+         for (int count = 1; count <= hour; count++) {
+             std::cout << "Cuc-koo! " << count << "\n";
+         }
+     }
+     if (hour < 0 || hour > 24) {
+         std::cout << "You entered an invalid value\n";
+     }
 }
