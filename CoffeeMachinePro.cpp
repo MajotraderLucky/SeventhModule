131acf0752c57899c14ac7b242fbc4d6ba96b393
Sergey_Ryazanov <majo78@mail.ru>
2021-10-05 14:22:29 +0300

    Add file CoffeeMachinePro.cpp

--- CoffeeMachinePro.cpp
+++ CoffeeMachinePro.cpp
@@ -9,15 +9,43 @@ int main() {
     std::cout << "How much milk to pour?\n";
     std::cout << "---> ";
     std::cin >> milk;
-    bool americanoHave = (water >= 300);
-    bool latte = (water >= 30 && milk >= 270);
-       if (americanoHave == true) {
-           std::cout << "Coffee Machine have " << (water / 300) << " americano\n";
+    int countLatteMilk  = milk / 270;
+    int countLatteWater = water / 30;
+    int countAmericano = water / 300;
+    int countLatte = 0;
+    int countSellLatte = 0;
+    int countSellAmericano = 0;
+       if (countLatteMilk < countLatteWater) {
+           countLatte = countLatteMilk;
+       } else if (countLatteMilk > countLatteWater) {
+           countLatte = countLatteWater;
        }
-       if ((latte == true) && ((milk / 270) <= (water / 30))) {
-           std::cout << "Coffee Machine have " << (milk / 270) << " latte\n";
-       } else if  ((latte == true) && ((milk / 270) >= (water / 30))) {
-           std::cout << "Coffee Machine have " << (water / 30) << " latte\n";
+       bool haveDrinks = (countLatte > 0) && (countAmericano > 0);
+       for (int count = 0; haveDrinks == true; count++) {
+           if (countAmericano >= 0 && countLatte >= 0) {
+               std::cout << "latte - " << countLatte << "\n";
+               std::cout << "americano - " << countAmericano << "\n";
+               std::cout << "for-----latte - push '1'\n";
+               std::cout << "for americano - push '2'\n";
+               std::cout << "---> ";
+               int choice;
+               std::cin >> choice;
+               if (choice == 1) {
+                   milk -= 270;
+                   water -= 30;
+                   countSellLatte++;
+                   if (countLatte != 0) {
+                       countLatte--;
+                   }
+               } else if (choice == 2) {
+                   water -= 300;
+                   countSellAmericano++;
+                   if (countAmericano != 0) {
+                       countAmericano--;
+                   }
+               }
+           }
        }
-
+       std::cout << "water = " << water << "\n";
+       std::cout << "milk  = " << milk << "\n";
 }
