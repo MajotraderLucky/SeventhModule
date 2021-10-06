c5e609faeefbfbbb6b32f0ff6d98ea8cf47e5578
Sergey_Ryazanov <majo78@mail.ru>
2021-10-06 19:07:31 +0300

    Change file MarsRover.cpp

--- MarsRover.cpp
+++ MarsRover.cpp
@@ -10,12 +10,37 @@ int main() {
     std::cout << "Backward movement - push 's'\n";
     std::cout << "Left     movement - push 'a'\n";
     std::cout << "Right    movement - push 'd'\n";
-    std::cout << "---> ";
-    std::string move;
-    std::cin >> move;
-       for (move = "w"; y <= longRoom; ) {
-           y++;
-           std::cout << "The mars rover moved on = y:" << y << " - x" << x << "\n";
-           break;
+       for (; true;) {
+           std::cout << "---> ";
+           std::string move;
+           std::cin >> move;
+           if (move == "w" && y <= 14) {
+               y++;
+               std::cout << "y:" << y << " - x" << x << "\n";
+           } else if (move == "w" && y >= 15) {
+               std::cout << "This is a wall forward!\n";
+               std::cout << "y:" << y << " - x" << x << "\n";
+           }
+           if (move == "s" && y >= 2) {
+               y--;
+               std::cout << "y:" << y << " - x" << x << "\n";
+           } else if (move == "s" && y < 2){
+               std::cout << "This is a wall behind!\n";
+               std::cout << "y:" << y << " - x" << x << "\n";
+           }
+           if (move == "a" && x >= 2) {
+               x--;
+               std::cout << "y:" << y << " - x" << x << "\n";
+           } else if (move == "a" && x < 2) {
+               std::cout << "This is a wall left!\n";
+               std::cout << "y:" << y << " - x" << x << "\n";
+           }
+           if (move == "d" && x <= 19) {
+               x++;
+               std::cout << "y:" << y << " - x" << x << "\n";
+           } else if (move == "d" && x > 19) {
+               std::cout << "This is a wall right!\n";
+               std::cout << "y:" << y << " - x" << x << "\n";
+           }
        }
 }
